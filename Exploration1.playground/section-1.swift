73b8d9b27afd762ee13201acc22eeffe0c1f945a
Jason Robinson <Jason@MUIT-MBP13.local>
2014-09-10 17:04:09 -0500

    Exploration1 Code

---
+++ Exploration1.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let x = 5
+
+x + 5
+
+let name = "Jason"
+
+for value in name {
+    if (value == "J"){
+        println("hello")
+    }
+    else {
+        println (name)
+    }
+}
